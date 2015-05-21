fa927e00be57d0553b653d80550ef39d4c3f3faa
emurphy <emmet@dealbase.com>
2015-05-21 15:22:18 -0700

    smooth PE10 series (adjust yyyy.1 to yyyy.10)

--- shiller_sp.js
+++ shiller_sp.js
@@ -14,7 +14,7 @@ module.exports.sp_pe10 = function (start_year, start_month) {
 
         if (typeof date !== 'undefined' && typeof pe_ratio !== 'undefined') {
             if (typeof start_year === 'undefined' || date_after(start_year, start_month, date.v)) {
-                series.observations.push({date: date.v.toString(), value: pe_ratio.v});
+                series.observations.push({date: format_date(date.v.toString()), value: pe_ratio.v});
             }
         }
         series.count = series.observations.length;
@@ -32,14 +32,29 @@ function date_after(start_year, start_month, year_dot_month) {
             return true;
         }
         else {
-            var month = Math.ceil((year_dot_month - year) * 100);
-            return month >= start_month;
+            return parseInt(month(year_dot_month)) >= start_month;
         }
     }
     else {
         return false;
     }
 
+}
+
+function month(year_dot_month) {
+    var month = year_dot_month.split(".")[1];
+    if (month == '1') {
+        month = '10';
+    }
+    return month;
+}
+
+function year(year_dot_month) {
+    return year_dot_month.split(".")[0];
+}
+
+function format_date(year_dot_month) {
+    return year(year_dot_month) + '.' +  month(year_dot_month);
 }
 
 
